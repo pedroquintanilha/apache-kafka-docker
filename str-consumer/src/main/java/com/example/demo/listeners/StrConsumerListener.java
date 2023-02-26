c094e693011e35fae05c3be2ff18f39d4b25c790
Pedro Quintanilha <pedrosquinta@gmail.com>
2023-02-26 14:43:00 -0300

    TopicPartition

--- str-consumer/src/main/java/com/example/demo/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/example/demo/listeners/StrConsumerListener.java
@@ -3,6 +3,7 @@ package com.example.demo.listeners;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 
@@ -12,9 +13,21 @@ public class StrConsumerListener {
 
 	private static final Logger log = LogManager.getLogger(StrConsumerListener.class);
 	
-	@KafkaListener(groupId = "group-1", topics= "str-topic", containerFactory = "strContainerFactory")
-	public void listener(String message) {
-		log.info("Receive message {}", message);
+	@KafkaListener(groupId = "group-1",
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"0"})
+			}
+			, containerFactory = "strContainerFactory")
+	public void create(String message) {
+		log.info("CREATE ::: Receive message {}", message);
 	}
 	
+	@KafkaListener(groupId = "group-1",
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"1"})
+			}
+			, containerFactory = "strContainerFactory")
+	public void log(String message) {
+		log.info("LOG ::: Receive message {}", message);
+	}
 }
--- str-producer/src/main/java/com/example/demo/services/StringProducerService.java
+++ str-producer/src/main/java/com/example/demo/services/StringProducerService.java
@@ -5,7 +5,6 @@ package com.example.demo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
